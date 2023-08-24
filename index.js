7b4115dc14c286ced5cfb0824bd5e7aa51c00e91
Jacob <weaver.jacob.t@gmail.com>
2023-08-24 16:30:59 -0500

    thanks for the tip

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if(someValue < 400){ 
+    return 'This one is on me!';  
+  }
+  else if (someValue >= 400 && someValue <= 2000 ) {
+    return 'That will be twenty bucks.';
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500) {
+    return 'No can do.';
+  }
+}
+function ternaryCheckCity(someCity){
+  if (someCity == 'NYC') {
+    return "Ok, sounds good.";
+  }
+  else if (someCity !== 'NYC') {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+    if (tip == 'generous') {
+      return "Thank you so much.";
+    }
+    else if (tip == 'not as generous') {
+      return "Thank you.";
+    }
+    else {
+      return "Bye."
+    }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
